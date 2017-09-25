395efbe9c1adfc60a72635a3e4faed320705ab7d
Dmitriy Avdeenko <dmitriyavdeenko@MacBook-Air-spbiphones.local>
2017-09-25 20:53:05 +0300

    исправлен баг

--- password_strength.py
+++ password_strength.py
@@ -1,27 +1,26 @@
-# -*- coding: utf-8 -*-
 import re
 
-def case_sensitivity_test(password):
+def get_result_of_case_sensitivity_test(password):
     if (password.upper() != password) and (password.lower() != password):
         return 2
     return 0
 
-def digits_test(password):
+def get_result_of_digits_test(password):
     if re.search('[0-9]', password):
         return 2
     return 0
 
-def special_symbols_test(password):
+def get_result_of_special_symbols_test(password):
     if re.search('[@ # \$ ! \. \* \? \/ <  \\ >]', password):
         return 2
     return 0
 
-def correct_password_test(password):
+def get_result_of_correct_password_test(password):
     if re.search('[^ a-zA-Z0-9_@#\$!\.\*\?\/<\\>]', password):
         return False
     return True
 
-def password_length_test(password):
+def get_result_of_password_length_test(password):
     if len(password) <= 8:
         return 0
     elif len(password) <= 12:
@@ -29,7 +28,7 @@ def password_length_test(password):
     else:
         return 3
 
-def is_popular_password(password):
+def get_result_of_popular_password_test(password):
     f = open('popular_password.txt')
     for line in f:
         if line == password:
@@ -38,18 +37,18 @@ def is_popular_password(password):
     
     
 def get_password_strength(password):
-    rating = 1
-    rating += case_sensitivity_test(password) + digits_test(password) + special_symbols_test(password) + password_length_test(password)
-    if (not password_length_test(password)) or is_popular_password(password):
+    password_rating = 1
+    password_rating += get_result_of_case_sensitivity_test(password) + get_result_of_digits_test(password) + get_result_of_special_symbols_test(password) + get_result_of_password_length_test(password)
+    if (not get_result_of_password_length_test(password)) or get_result_of_popular_password_test(password):
         rating = 1
-    return rating
+    return password_rating
     
     
 
 if __name__ == '__main__':
     while True:
         password = input() 
-        if not correct_password_test(password):
+        if not get_result_of_correct_password_test(password):
             print("Incorrect formart of password, try again")    
         else:
             break

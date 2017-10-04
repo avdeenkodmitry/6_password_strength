3e0de47c55b870e0c5c1d20e04826aec8a5fa200
Dmitriy Avdeenko <dmitriyavdeenko@MacBook-Air-spbiphones.local>
2017-10-04 21:39:30 +0300

    all comments fixed

--- password_strength.py
+++ password_strength.py
@@ -1,4 +1,5 @@
 import re
+import getpass
 
 def get_result_of_case_sensitivity_test(password):
     if (password.upper() != password) and (password.lower() != password):
@@ -16,9 +17,8 @@ def get_result_of_special_symbols_test(password):
     return 0
 
 def get_result_of_correct_password_test(password):
-    if re.search('[^ a-zA-Z0-9_@#\$!\.\*\?\/<\\>]', password):
-        return False
-    return True
+    return (not re.search('[^ a-zA-Z0-9_@#\$!\.\*\?\/<\\>]', password))
+    
 
 def get_result_of_password_length_test(password):
     if len(password) <= 8:
@@ -29,17 +29,18 @@ def get_result_of_password_length_test(password):
         return 3
 
 def get_result_of_popular_password_test(password):
-    file_with_popular_password = open('popular_password.txt')
-    for line in file_with_popular_password:
-        if line == password:
-            return True
-    return False
+    with open('popular_password.txt','r') as file_with_popular_password:
+        return (password in file_with_popular_password)    
     
     
 def get_password_strength(password):
     password_rating = 1
-    password_rating += get_result_of_case_sensitivity_test(password) + get_result_of_digits_test(password) + get_result_of_special_symbols_test(password) + get_result_of_password_length_test(password)
-    if (not get_result_of_password_length_test(password)) or get_result_of_popular_password_test(password):
+    password_rating += get_result_of_case_sensitivity_test(password) +\
+                       get_result_of_digits_test(password) + \
+                       get_result_of_special_symbols_test(password) + \
+                       get_result_of_password_length_test(password)
+    if (not get_result_of_password_length_test(password)) or \
+            get_result_of_popular_password_test(password):
         password_rating = 1
     return password_rating
     
@@ -47,9 +48,9 @@ def get_password_strength(password):
 
 if __name__ == '__main__':
     while True:
-        password = input() 
+        password = getpass.getpass() 
         if not get_result_of_correct_password_test(password):
-            print("Incorrect formart of password, try again")    
+            print("Incorrect format of password, try again")    
         else:
             break
     print("Your rating: ", get_password_strength(password))
